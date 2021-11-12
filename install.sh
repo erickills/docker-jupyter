494cb24d365cc69af008f9ae1b53d32bab3a28c3
ECS <38625756+erickills@users.noreply.github.com>
2021-11-12 11:51:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-# check if the docker installed
+# Nov. 9, 2021
+# by erickills
 
+# check if the docker installed
 docker --version 
 
 # pull the image container
-
 docker pull eipdev/alpine-jupyter-notebook
 
 # run the container at port 8888
-
 docker run -d -p 8888:8888 -v /path/to/notebooks:/opt/notebook eipdev/alpine-jupyter-notebook
